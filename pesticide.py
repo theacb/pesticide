42b4d36be35c5a4bb769a563bd374347de20d7da
Avery Brown <avery@averychristianbrown.com>
2016-04-23 22:31:04 -0400

    Beta functionality Script now prints out a log containing the bad nodes

--- pesticide.py
+++ pesticide.py
@@ -29,14 +29,14 @@ def get_log_file(osx_path, win_path):
     else:
         raise OSError('Unsupported Operating System')
 
-    log_name = "lettuce_{0}-{1}.log".format(getuser(),
-                                            strftime("%y%m%d-%H.%M.%S")
-                                            )
+    log_name = "pesticide_{0}-{1}.log".format(getuser(),
+                                              strftime("%y%m%d-%H.%M.%S")
+                                              )
 
     return os.path.normpath(os.path.join(log_folder_path, log_name))
 
 
-def setup_log():
+def setup_log(level='logging.WARNING'):
     """
     Sets up logging to a file
     :return: the logging object
@@ -46,7 +46,7 @@ def setup_log():
 
     # Log setup
     lg = logging.getLogger("pesticide")
-    lg.setLevel(eval('logging.DEBUG'))
+    lg.setLevel(eval(level))
 
     fh = logging.FileHandler(get_log_file(osx_log_path, win_log_path))
     formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
@@ -81,10 +81,12 @@ def search_file(f, bad_types):
 
     flg.info("Looking in file: {}".format(f.get_name()))
 
-    mc.file(f.get_path(), open=True)
+    mc.file(f.get_path(),
+            open=True
+            )
 
     flg.info("Getting Node List")
-    nodes = mc.ls(l=True)
+    nodes = mc.ls(shortNames=True)
 
     flg.info("Checking Nodes")
     bad_nodes = check_nodes(nodes, bad_types)
@@ -99,6 +101,11 @@ def search_file(f, bad_types):
 def check_nodes(nodes, bad_types):
     flg = logging.getLogger("pesticide.check_nodes")
 
+    flg.info("Removing References")
+    for n in nodes:
+        if ':' in n:
+            nodes.remove(n)
+
     flg.info("Sorting Nodes")
     nodes.sort()
     bad_types.sort()
@@ -110,23 +117,24 @@ def check_nodes(nodes, bad_types):
 
     flg.debug("Checking for bad nodes that are definitely not present")
     for n in bad_types:
-        if not n.lower()[0] in index_dict:
-            flg.debug("No nodes start with {0}, removing: {1}".format(n.lower()[0], n))
+        if not n[0] in index_dict:
+            flg.debug("No nodes start with {0}, removing: {1}".format(n[0], n))
             bad_types.remove(n)
 
     for n in bad_types:
         flg.debug("Looking for {} nodes in scene".format(n))
-        cur_char = n.lower()[0]
+        cur_char = n[0]
         ind = (index_dict[cur_char])[0]
 
         loop = True
 
         while loop:
-            if cur_node.lower()[0] != cur_char:
-                break
             cur_node = nodes[ind]
 
-            if cur_node == n:
+            if cur_node[0] != cur_char:
+                break
+
+            if n in cur_node:
                 flg.debug("{0} is of type {1}".format(cur_node, n))
                 bad_nodes.append(cur_node)
 
@@ -142,14 +150,14 @@ def find_alphabetical_index(nodes):
     nodes.sort()
     index_dict = {}
 
-    possible_chars = list("0123456789_abcdefghijklmnopqrstuvwxyz")
+    possible_chars = list("0123456789_ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
     master_cur_char = 0
     i = 0
 
     flg.debug("Getting alphabetical index of node list")
     for n in nodes:
         flg.debug("Node: {}".format(n))
-        this_cur_char = n.lower()[0]
+        this_cur_char = n[0]
         for c in range(master_cur_char, len(possible_chars)):
             if possible_chars[c] == this_cur_char:
                 flg.debug("Node {0} has the first name to begin with {1} at index {2}".format(n, possible_chars[c], i))
@@ -163,21 +171,142 @@ def find_alphabetical_index(nodes):
 
 
 def bad_types_list():
-    return ['rman']
+    return ['rman', 'renderManRISGlobals']
 
 
-def compile_results(results):
-    return results
+def compile_results(file_objects, *args):
+    flg = logging.getLogger("pesticide.compile_results")
+
+    results_for_print = []
+
+    if len(file_objects) <= 0:
+        flg.debug("No file objects passed to function")
+        results_for_print.append("No Files Processed")
+        return
+    for f in file_objects:
+        bad_nodes = f.get_bad_nodes()
+        if len(bad_nodes) > 0:
+            msg = "-Bad node{0} found in file: {1}".format(is_plural(bad_nodes),
+                                                           f.get_name(),
+                                                           )
+            results_for_print.append('\r')
+            results_for_print.append(msg)
+
+            flg.warning(msg)
+
+            line_str = " " * 4
+
+            for b in f.get_bad_nodes():
+                msg = "{0}-{1}".format(" " * 4, b)
+                flg.warning(msg)
+                line_str = "{0}{1}, ".format(line_str, b)
+                if len(line_str) > 150:
+                    results_for_print.append(line_str)
+                    line_str = " " * 4
+
+            msg = "Total: {}".format(len(bad_nodes))
+
+            flg.warning(msg)
+
+            results_for_print.append(msg)
+
+        else:
+            flg.debug("No bad nodes found in file: {}".format(f.get_name()))
+
+    if len(results_for_print) <= 0:
+        results_for_print.append("Searched {} files\r".format(len(file_objects)))
+        results_for_print.append("No bad nodes found")
+    else:
+        msg = "Results:"
+        results_for_print.insert(0, msg)
+
+        msg = "Searched {} files\r".format(len(file_objects))
+        flg.debug(msg)
+        results_for_print.insert(0, msg)
+
+    if len(args) > 0:
+        for a in reversed(args):
+            results_for_print.insert(0, a)
+
+    return results_for_print
+
+
+def is_plural(array):
+    if len(array) == 1:
+        return ""
+    else:
+        return "s"
+
+
+def array_to_file(results_array, name):
+    flg = logging.getLogger("pesticide.array_to_file")
+    try:
+        f = open(name, 'w')
+
+        flg.debug("Writing to file: {}".format(name))
+        flg.debug(f)
+
+        for line in results_array:
+            f.write("{}\r".format(line))
+
+        f.close()
+        flg.debug("Done")
+    except IOError as e:
+        flg.error("Unable to write results to file")
+        flg.error("IO Error: {}".format(e))
+        raise
+
+
+def test_file(name):
+    flg = logging.getLogger("pesticide.test_file")
+    try:
+        f = open(name, 'w')
+
+        flg.debug("Creating file: {}".format(name))
+        flg.debug(f)
+
+        f.close()
+        flg.debug("Done")
+    except IOError as e:
+        flg.error("Unable to create file")
+        flg.error("IO Error: {}".format(e))
+        raise
+
+
+def gen_file_name():
+
+    path = ""
+
+    osx_path = '/Volumes/digm_anfx/SPRJ_cgbirds/_production/scripts/logs/pesticide'
+    win_path = '//awexpress.westphal.drexel.edu/digm_anfx/SPRJ_cgbirds/_production/scripts/logs/pesticide'
+
+    if sys.platform == "darwin":
+        # OS X
+        path = osx_path
+
+    elif sys.platform == "win32":
+        # Windows...
+        path = win_path
+
+    file_name = "results_{0}-{1}.txt".format(getuser(),
+                                             strftime("%y%m%d-%H.%M.%S")
+                                             )
+    return os.path.join(path, file_name)
 
 
 def main():
+    start_time = strftime("%H:%M:%S")
+    date = strftime("%m/%d/%y")
+
     flg = setup_log()
 
-    search_dir = '//awexpress.westphal.drexel.edu/digm_anfx/SPRJ_cgbirds/_production/assets/environment\elements\curves'
+    search_dir = '//awexpress.westphal.drexel.edu/digm_anfx/SPRJ_cgbirds'
 
     # Starts interpreter
     mcs.initialize(name='python')
     flg.info("Maya Initialized")
+
+    output_file_name = gen_file_name()
 
     files = get_ma_files(os.path.normpath(search_dir))
 
@@ -187,11 +316,24 @@ def main():
     bad_types = bad_types_list()
 
     flg.info("Searching Files")
+    i = 0
     for f in files:
+        print("{} out of {} files".format(i, len(files)))
         results.append(search_file(f, bad_types))
+        i += 1
 
     flg.info("Compiling results")
-    compile_results(results)
+    comp_res = compile_results(results,
+                               "Searched: {}".format(search_dir),
+                               '\r',
+                               "Started at {} on {}".format(start_time, date),
+                               "User: {}".format(getuser()),
+                               '\r',
+                               '*' * 150,
+                               '\r'
+                               )
+
+    array_to_file(comp_res, output_file_name)
 
     mcs.uninitialize()
     flg.info("Maya uninitialized")
